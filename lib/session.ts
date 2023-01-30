b5c697a5bdbe5f745ed155d928e7032694907d2e
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2023-01-30 15:11:59 -0300

    HTTPs iron-session

--- lib/session.ts
+++ lib/session.ts
@@ -7,7 +7,7 @@ export default function withSession(handler: any) {
     cookieOptions: {
       // the next line allows to use the session in non-https environments like
       // Next.js dev mode (http://localhost:3000)
-      secure: process.env.NODE_ENV === "production",
+      secure: false // TODO should be set as process.env.NODE_ENV === "production" when SSL certificates are installed
     },
   });
 }
