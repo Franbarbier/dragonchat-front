d67893aa170c6e57abdbdaee7e0b7bc6325fbbd1
Francisco Barbier <ffranbarbier@gmail.com>
2023-04-07 12:16:58 -0300

    testeos con la api

--- components/cards/Card/FreeCard2.mocks.ts
+++ components/cards/Card/FreeCard2.mocks.ts
@@ -1,11 +1,14 @@
 import { IFreeCard2 } from './MessageFree';
 
 const base: IFreeCard2 = {
-    sampleTextProp : 'string',
-    setActiveCard: () => {},
-    activeCard : 2,
-    mensaje : 'string',
-    setMensaje: (m) => {},
+    sampleTextProp : "string",
+    setActiveCard: (id) => {return false},
+    activeCard : 3,
+    mensaje : "string",
+    setMensaje: (msj) => {return false},
+    setSelectedSecuence:  (seq) => {return false},
+    selectedSecuence : null,
+    setReadyMessage : (ready) => {return false}
 }
 
 export const mockFreeCard2Props = {
