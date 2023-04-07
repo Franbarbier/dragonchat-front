6061d24a5ba3da77c71eb513937f2109f0d79cf3
Francisco Barbier <ffranbarbier@gmail.com>
2023-04-06 21:29:54 -0300

    merge con premConv to test

--- components/cards/Card/FreeCard2.mocks.ts
+++ components/cards/Card/FreeCard2.mocks.ts
@@ -1,11 +1,14 @@
 import { IFreeCard2 } from './MessageFree';
 
 const base: IFreeCard2 = {
-    sampleTextProp : 'string',
-    setActiveCard: () => {},
+    sampleTextProp : "string",
+    setActiveCard: (num) => {return false;} ,
     activeCard : 2,
-    mensaje : 'string',
-    setMensaje: (m) => {},
+    mensaje : "string",
+    setMensaje: (msj) => {return false;},
+    setSelectedSecuence: (num) => {return false;} ,
+    selectedSecuence : null,
+    setReadyMessage : (ready) => {return false;} 
 }
 
 export const mockFreeCard2Props = {
