51e05cc03683d4ea331dee432e9c37a0cb04dfcd
Francisco Barbier <ffranbarbier@gmail.com>
2023-05-02 16:26:36 -0300

    avances para mergear con dev

--- components/cards/Card/FreeCard2.mocks.ts
+++ components/cards/Card/FreeCard2.mocks.ts
@@ -2,13 +2,14 @@ import { IFreeCard2 } from './MessageFree';
 
 const base: IFreeCard2 = {
     sampleTextProp : "string",
-    setActiveCard: (id) => {return false},
+    setActiveCard: (id: number) => {return false},
     activeCard : 3,
     mensaje : "string",
     setMensaje: (msj) => {return false},
-    setSelectedSecuence:  (seq) => {return false},
+    setSelectedSecuence:  (secuence) => {return false},
     selectedSecuence : null,
-    setReadyMessage : (ready) => {return false}
+    setReadyMessage : (ready) => {return false},
+    setBreadcrumb : (breadcrumb) => {return false},
 }
 
 export const mockFreeCard2Props = {
