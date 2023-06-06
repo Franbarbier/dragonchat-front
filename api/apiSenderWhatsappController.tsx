306aaa3af314691936c894e786cdf6c55475fbf8
NicolasArias1 <ariasespinoza23@gmail.com>
2023-06-06 12:06:27 -0400

    improve headers code

--- api/apiSenderWhatsappController.tsx
+++ api/apiSenderWhatsappController.tsx
@@ -5,39 +5,46 @@ const API_URL = process.env.NEXT_PUBLIC_API_SENDER_URL;
 
 const apiSenderWhatsappController = {
     unlinkWhatsapp: async (cookie: CookieValueTypes) => {
-        const accessToken = cookie && JSON.parse(String(cookie)).access_token
-        const url = `${API_URL}/client/close_client`;
-        const response = await fetch(url, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                'Authorization': `Bearer ${accessToken}`,
-            },
-        });
-        if (response.status == 200) {
-            alert("Whatsapp correctamente desvinculado.");
-        } else {
-            alert("Tu sesión no pudo ser desvinculada de forma correcta. Espera unos momentos y vuelve a intentar.");
-        }
+        try {
+            const accessToken = cookie && JSON.parse(String(cookie)).access_token
+            const url = `${API_URL}/client/close_client`;
+            const response = await fetch(url, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': `Bearer ${accessToken}`,
+                },
+            });
+            if (response.status == 200) {
+                alert("Whatsapp correctamente desvinculado.");
+            } else {
+                alert("Tu sesión no pudo ser desvinculada de forma correcta. Espera unos momentos y vuelve a intentar.");
+            }
 
-        return response;
+            return response;
+        } catch (error: any) {
+            return error
+        }
     },
     sendMessage: async (userId, receiverName, message, receiverNumber, cookie: CookieValueTypes) => {
         try {
             const accessToken = cookie && JSON.parse(String(cookie)).access_token
-            const headers = new Headers({
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${accessToken}`,
-            });
-
             const payload = { user: userId, name: receiverName, message: message, number: receiverNumber };
-            const response = await axios.post(`${API_URL}/message/send-basic`, payload, { headers: Object.fromEntries(headers) });
+            const response = await axios.post(
+                `${API_URL}/message/send-basic`,
+                payload,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${accessToken}`,
+                    }
+                }
+            );
 
             return response
         } catch (error: any) {
             return error
         }
-
     }
 }
 
