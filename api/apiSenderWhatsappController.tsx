31a256122a16dfe353f61a5d8de1c41c6084245c
NicolasArias1 <ariasespinoza23@gmail.com>
2023-06-07 17:09:08 -0400

    base

--- api/apiSenderWhatsappController.tsx
+++ api/apiSenderWhatsappController.tsx
@@ -1,33 +1,37 @@
 import axios from 'axios';
 
-const apiUrl = process.env.NEXT_PUBLIC_API_SENDER_URL;
-const messageUrl = `${apiUrl}/message`;
+const API_URL = process.env.NEXT_PUBLIC_API_SENDER_URL;
 
 const apiSenderWhatsappController = {
-    unlinkWhatsapp: async (userId) => {
-        const url = `${apiUrl}/client/close_client/${userId}`;
+    unlinkWhatsapp: async (authToken: string) => {
+        const url = `${API_URL}/client/close_client`;
         const response = await fetch(url, {
-            method: "PUT"
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${authToken}`,
+            },
         });
         if (response.status == 200) {
             alert("Whatsapp correctamente desvinculado.");
         } else {
             alert("Tu sesión no pudo ser desvinculada de forma correcta. Espera unos momentos y vuelve a intentar.");
         }
+
         return response;
     },
-    sendMessage: async (userId, receiverName, message, receiverNumber) => {
-        try{
-            const config = {
-                headers: {
-                    "Accept": "/",
-                    "Content-Type": "application/json"
-                }
-            }
+    sendMessage: async (userId, receiverName, message, receiverNumber, authToken: string) => {
+        try {
+            const headers = new Headers({
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${authToken}`,
+            });
+
             const payload = { user: userId, name: receiverName, message: message, number: receiverNumber };
-            const response = await axios.post(`${messageUrl}/send-basic`, payload, config);
+            const response = await axios.post(`${API_URL}/message/send-basic`, payload, { headers: Object.fromEntries(headers) });
+
             return response
-        }catch(error:any){
+        } catch (error: any) {
             return error
         }
 
