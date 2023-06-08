aaa26d5b46b35c63ae2eb9873c2ec76a38a100c7
Francisco <ffranbarbier@gmail.com>
2023-06-08 14:15:10 -0500

    descomment hide sign up

--- components/LoginView/LoginView.tsx
+++ components/LoginView/LoginView.tsx
@@ -112,8 +112,7 @@ const LoginView: React.FC<ILoginView> = ({ setNotification, notification }) => {
                     </button>
                 </div>
 
-                {/* Esto se esconde temporalmente hasta que se active el registre libre de usuarios */}
-                {/* <div className={styles.noAccount}>
+                <div className={styles.noAccount}>
                     <hr />
                     
                     <div>
@@ -122,7 +121,7 @@ const LoginView: React.FC<ILoginView> = ({ setNotification, notification }) => {
                             <Link href='/signup'>Regístrate</Link>
                         </button>
                     </div>
-                </div> */}
+                </div>
 
             </form>
         </div>    
