123fffe1d843dc0985b797fbf019784a4a19a286
NicolasArias1 <ariasespinoza23@gmail.com>
2023-06-09 23:22:10 -0400

    minor fix

--- middleware.ts
+++ middleware.ts
@@ -16,7 +16,7 @@ export async function middleware(req: NextRequest) {
   }
 
   if (authCookie) {
-    const accessToken = JSON.parse(authCookie!.value).access_token;
+    const accessToken = JSON.parse(authCookie.value).access_token;
 
     const apiResponse = await fetch(
       `${process.env.NEXT_PUBLIC_API_USER_URL}/ws`,
