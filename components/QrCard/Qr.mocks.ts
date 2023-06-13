3d0e9afd58c032b1267fb85f7873406647655426
Francisco <ffranbarbier@gmail.com>
2023-06-13 14:55:19 -0600

    pequena correccion

--- components/QrCard/Qr.mocks.ts
+++ components/QrCard/Qr.mocks.ts
@@ -1,7 +1,6 @@
 import { IQrCard } from './QrCard';
 
 const base: IQrCard = {
-    qr_url: 'https://qrcg-free-editor.qr-code-generator.com/main/assets/images/websiteQRCode_noFrame.png',
     linked_whatsapp: true
 }
 
