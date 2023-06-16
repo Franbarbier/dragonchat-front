b6744f12565ca486178f91f672cd74fe0ce5836c
Francisco <ffranbarbier@gmail.com>
2023-06-16 11:02:11 -0600

    PR review

--- components/Err404/NotFound.tsx
+++ components/Err404/NotFound.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import styles from './NotFound.module.css';
 
-interface INotFound {
 
-}
 
-const NotFound: React.FC<INotFound> = ({ }) => {
+const NotFound: React.FC= () => {
 
     return (
         <div className={styles.errCont}>
             <div>
                 <h4>Algo salio mal! No encontramos la pagina :(</h4>
                 <h6>Error</h6>
-                {/* <h3>404</h3> */}
                 <div>
                     <h2>4</h2>
                     <img src={'/dragon_anim.gif'} alt="dragon-chat"/>
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,11 @@
 import NotFound from "../components/Err404/NotFound";
 import PrimaryLayout from "../components/layouts/primary/PrimaryLayout";
-import { NextPageWithLayout } from "./page";
-import { GralProps } from "./_app";
 
-const NotFoundView : NextPageWithLayout<GralProps> = () => {
+const NotFoundView = () => {
 
     return (
         <section>
-            {/* <NotFound /> */}
             <NotFound />
-
-            {/* <MainCont width={90} maxWidth={400}>
-                <h1>404</h1>
-            </MainCont> */}
         </section>
     );
 };
