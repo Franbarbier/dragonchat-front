48bdbc0f7068f79542ca940b7f859d3de5384a44
Alfredo Puerta <alfredopuerta.cl@gmail.com>
2023-10-29 13:18:11 -0400

    hidden button whatsapp

--- components/WppBtn/WppBtn.tsx
+++ components/WppBtn/WppBtn.tsx
@@ -1,6 +1,4 @@
-import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
-import styles from './WppBtn.module.css';
 
 
 export interface IWppBtn {
@@ -15,24 +13,26 @@ const WppBtn: React.FC<IWppBtn> = ({  }) => {
     const [renderWppMsj, setRenderWppMsj] = useState<boolean>(false);
 
     return (
-            <div id={styles.wpp_btn_cont}>
-                <div>
-                    <div>
-                        <a href="https://wa.me/5491156958765?text=Hola!%20Me%20contacto%20desde%20dragonchat%20por%20" target="_blank">
-                            <img src="/whatsapp.png" />
-                        </a>
-                    </div>
-                    <AnimatePresence>
-                    {renderWppMsj &&
-                            <motion.span
-                                initial={{ opacity: 0, scale : 0.9, y : '-50%' }}
-                                animate={{ opacity: 1, scale : 1, y : '-50%' }}
-                                exit={{ opacity: 0, scale : 0.9, y : '-50%' }}
-                            >Sacate cualquier duda de la plataforma! ❓😊</motion.span>
-                    }
-                    </AnimatePresence>
-                </div>
-            </div>
+            // <div id={styles.wpp_btn_cont}>
+            //     <div>
+            //         <div>
+            //             <a href="https://wa.me/5491156958765?text=Hola!%20Me%20contacto%20desde%20dragonchat%20por%20" target="_blank">
+            //                 <img src="/whatsapp.png" />
+            //             </a>
+            //         </div>
+            //         <AnimatePresence>
+            //         {renderWppMsj &&
+            //                 <motion.span
+            //                     initial={{ opacity: 0, scale : 0.9, y : '-50%' }}
+            //                     animate={{ opacity: 1, scale : 1, y : '-50%' }}
+            //                     exit={{ opacity: 0, scale : 0.9, y : '-50%' }}
+            //                 >Sacate cualquier duda de la plataforma! ❓😊</motion.span>
+            //         }
+            //         </AnimatePresence>
+            //     </div>
+            // </div>
+
+            <div></div>
     
     );
 }
