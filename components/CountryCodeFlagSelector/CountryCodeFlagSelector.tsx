142165e768c2360346463d46959ad61503ce9bc9
NicolasArias1 <ariasespinoza23@gmail.com>
2023-11-05 19:37:03 -0300

    base

---
+++ components/CountryCodeFlagSelector/CountryCodeFlagSelector.tsx
@@ -0,0 +1,46 @@
+import { Dispatch, SetStateAction, useCallback } from 'react';
+import styles from './CountryCodeFlagSelector.module.css';
+import { countries } from './data.json';
+
+interface CountryCodeFlagSelectorProps {
+    phone: { code: string, number: string },
+    setPhone: Dispatch<SetStateAction<{ code: string, number: string }>>,
+};
+
+const CountryCodeFlagSelector: React.FC<CountryCodeFlagSelectorProps> = (props) => {
+    const { phone, setPhone } = props;
+
+    const handlePhone = useCallback((value: string) => {
+        if ((/^\d+$/.test(value) || value === '') && value.length !== 15) {
+            setPhone(prev => ({ ...prev, number: value }));
+        }
+    }, [setPhone])
+
+    return (
+        <div className={styles.container}>
+            <select
+                className={styles.select}
+                onChange={({ target: { value } }) => setPhone(prev => ({ ...prev, code: value }))}
+            >
+                {countries.map(c => (
+                    <option 
+                        key={c.code_2} 
+                        value={c.dial_code}
+                    >
+                        {`${c.emoji} ${c.code_3} ${c.dial_code}`}
+                    </option>
+                ))}
+            </select>
+            <input
+                className={styles.phone}
+                placeholder="89898989"
+                type="text"
+                value={phone.number}
+                onChange={({ target: { value } }) => handlePhone(value)}
+                autoComplete="off"
+            />
+        </div>
+    );
+}
+
+export default CountryCodeFlagSelector;
--- components/SignUpView/SignUpView.tsx
+++ components/SignUpView/SignUpView.tsx
@@ -4,89 +4,91 @@ import { useEffect, useState } from 'react';
 import apiUserController from '../../api/apiUserController';
 import { LOGIN_COOKIE } from '../../constants/index';
 import { STATUS } from '../../enums';
-import CardTitle from '../cards/CardTitle/CardTitle';
+import CountryCodeFlagSelector from '../CountryCodeFlagSelector/CountryCodeFlagSelector';
 import InputGral from '../InputGral/InputGral';
 import Loader from '../Loader/Loader';
 import { INotification } from '../Notification/Notification';
 import OrangeBtn from '../OrangeBtn/OrangeBtn';
+import CardTitle from '../cards/CardTitle/CardTitle';
 import styles from './SignUpView.module.css';
 
 export interface ISignUpView {
     stripe_data: Object | null,
-    setNotification : (notification : INotification) => void,
-    notification : INotification
+    setNotification: (notification: INotification) => void,
+    notification: INotification
 }
 
 const SignUpView: React.FC<ISignUpView> = ({ stripe_data, setNotification, notification }) => {
+    const [name, setName] = useState('');
+    const [mail, setMail] = useState('');
+    const [pass, setPass] = useState('');
+    const [confirmPass, setConfirmPass] = useState('');
+    const [phone, setPhone] = useState({code: '', number: ''});
+    const [equalPass, setEqualPass] = useState(true);
+    const [userExists, setUserExists] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    const [name, setName] = useState('')
-    const [mail, setMail] = useState('')
-    const [pass, setPass] = useState('')
-    const [confirmPass, setConfirmPass] = useState('')
-    const [equalPass, setEqualPass] = useState(true)
-    const [userExists, setUserExists] = useState(false)
+    useEffect(() => {
+        setLoading(false)
+    }, [userExists]);
 
-    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        Router.prefetch("/login");
+    }, []);
 
-    async function handleLogin(loginMail,loginPass ) {
-            const login_status = await apiUserController.login(loginMail, loginPass);
-            if ( login_status?.status == 200 || login_status?.status == 201 ) {
-                const login_storage = {
-                    access_token : login_status?.data.access_token,
-                    user_id : login_status?.data.user_id
+    async function handleLogin(loginMail, loginPass) {
+        const login_status = await apiUserController.login(loginMail, loginPass);
+        if (login_status?.status == 200 || login_status?.status == 201) {
+            const login_storage = {
+                access_token: login_status?.data.access_token,
+                user_id: login_status?.data.user_id
+            }
+            Cookies.set(
+                LOGIN_COOKIE,
+                JSON.stringify(login_storage), // secure flag option must be added in the future
+                {
+                    sameSite: 'strict'
                 }
-                Cookies.set(
-                    LOGIN_COOKIE,
-                    JSON.stringify(login_storage), // secure flag option must be added in the future
-                    {
-                      sameSite: 'strict'
-                    }
-                  );
-                    Router.push("/dash")
-                }
-                // Router.push("/login")
-    }  
-
-
-useEffect(() => {
-    setLoading(false)
-}, [userExists])
+            );
+            Router.push("/dash")
+        }
+    }
 
     async function handleCrearCuenta() {
         setLoading(true)
 
         if (equalPass) {
-            if( name != "" && mail != "" && pass != "" && confirmPass != "" ){
-                
+            if (name != "" && mail != "" && pass != "" && confirmPass != "") {
+
                 if (!validateEmail(mail)) {
                     setNotification({
-                        status : STATUS.ERROR,
-                        render : true,
-                        message : "El mail ingresado no es valido.",
-                        modalReturn : ()=>{setNotification({...notification, render : false })}
+                        status: STATUS.ERROR,
+                        render: true,
+                        message: "El mail ingresado no es valido.",
+                        modalReturn: () => { setNotification({ ...notification, render: false }) }
                     })
                     setLoading(false)
                     return false;
                 }
                 if (pass.length < 8) {
                     setNotification({
-                        status : STATUS.ERROR,
-                        render : true,
-                        message : "La contraseña debe tener al menos 8 caracteres.",
-                        modalReturn : ()=>{setNotification({...notification, render : false })}
+                        status: STATUS.ERROR,
+                        render: true,
+                        message: "La contraseña debe tener al menos 8 caracteres.",
+                        modalReturn: () => { setNotification({ ...notification, render: false }) }
                     })
                     setLoading(false)
                     return false;
                 }
-                    
+
 
                 const onSuccess = () => {
-                    if (signUp_res?.status == 200 || signUp_res?.status == 201 ) {
+                    if (signUp_res?.status == 200 || signUp_res?.status == 201) {
                         setNotification({
-                            status : STATUS.SUCCESS,
-                            render : true,
-                            message : "Usuario creado con exito!",
-                            modalReturn : ()=>{setNotification({...notification, render : false })}
+                            status: STATUS.SUCCESS,
+                            render: true,
+                            message: "Usuario creado con exito!",
+                            modalReturn: () => { setNotification({ ...notification, render: false }) }
                         })
                         handleLogin(mail, pass)
                     }
@@ -94,39 +96,36 @@ useEffect(() => {
 
                 const signUp_res = await apiUserController.signUp(name, mail, pass, confirmPass, setUserExists, stripe_data);
                 onSuccess()
-            }else{
+            } else {
                 setNotification({
-                    status : STATUS.ERROR,
-                    render : true,
-                    message : "Asegurate de completar todos los campos!",
-                    modalReturn : ()=>{setNotification({...notification, render : false })}
+                    status: STATUS.ERROR,
+                    render: true,
+                    message: "Asegurate de completar todos los campos!",
+                    modalReturn: () => { setNotification({ ...notification, render: false }) }
                 })
             }
-        }else{
+        } else {
             setNotification({
-                status : STATUS.ERROR,
-                render : true,
-                message : "Las contraseñas no coinciden!",
-                modalReturn : ()=>{setNotification({...notification, render : false })}
+                status: STATUS.ERROR,
+                render: true,
+                message: "Las contraseñas no coinciden!",
+                modalReturn: () => { setNotification({ ...notification, render: false }) }
             })
-
         }
     }
 
-    useEffect(() => {
-        Router.prefetch("/login");
-    })
 
-    useEffect(()=>{
+
+    useEffect(() => {
         if (confirmPass != '' && confirmPass != pass) {
             setEqualPass(false)
-        }else{
+        } else {
             setEqualPass(true)
         }
 
-    },[confirmPass, pass])
-    
-  
+    }, [confirmPass, pass])
+
+
     const validateEmail = (email) => {
         // Regular expression to validate email addresses
         const re = /\S+@\S+\.\S+/;
@@ -135,8 +134,8 @@ useEffect(() => {
 
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
-          // Trigger sign-up function when Enter key is pressed
-          handleCrearCuenta();
+            // Trigger sign-up function when Enter key is pressed
+            handleCrearCuenta();
         }
     };
 
@@ -150,49 +149,39 @@ useEffect(() => {
     return (
         <>
             <Loader loading={loading} />
-       
-        <div className={styles.signup_cont} >
-            <div>
-                <CardTitle text="Registrarse" />
 
-                {/* <div>
-                    <button className={styles.googleInit}>
-                        <img src="/buscar.png" width="18px" alt="find-image"/>
-                        <span>Continuar con Google</span>
-                    </button>
-                </div> */}
-                
-                <h6>Registrarte con tu email</h6>
+            <div className={styles.signup_cont} >
                 <div>
-                    <InputGral placeholder='Nombre' type="text" value={name} onChange={ setName } classes={['error']} />
-                    <InputGral placeholder='E-mail' type="email" value={mail} onChange={ setMail }/>
-                    <InputGral placeholder='Contraseña' type="password" value={pass} onChange={ setPass }/>
-                    <InputGral placeholder='Repetir contraseña' type="password" value={confirmPass} onChange={ setConfirmPass }/>
-                    {!equalPass &&
-                        <p className={styles.alert}>Las contraseñas no coinciden :(</p>
-                    }
-                    {userExists &&
-                        <p className={styles.alert}>Ya existe un usuario registrado con ese email.</p>
-                    }
-                </div>
+                    <CardTitle text="Registrarse" />
 
-                <OrangeBtn text="Crear cuenta" onClick={ handleCrearCuenta }/>
+                    <h6>Registrarte con tu email</h6>
+                    <InputGral placeholder='Nombre' type="text" value={name} onChange={setName} classes={['error']} />
+                    <InputGral placeholder='E-mail' type="email" value={mail} onChange={setMail} />
+                    <CountryCodeFlagSelector phone={phone} setPhone={setPhone} />
+                    <InputGral placeholder='Contraseña' type="password" value={pass} onChange={setPass} />
+                    <InputGral placeholder='Repetir contraseña' type="password" value={confirmPass} onChange={setConfirmPass} />
+                    {!equalPass && (
+                        <p className={styles.alert}>{"Las contraseñas no coinciden :("}</p>
+                    )}
+                    {userExists && (
+                        <p className={styles.alert}>{"Ya existe un usuario registrado con ese email."}</p>
+                    )}
 
-                <div className={styles.hasAccount}>
-                    <hr />
-                    
-                    <div>
-                        <span>Ya tienes una cuenta?</span>
-                        <button>
-                            <span onClick={()=> Router.push('/login') }>Iniciar sesión</span>
-                        </button>
+                    <OrangeBtn text="Crear cuenta" onClick={handleCrearCuenta} />
+
+                    <div className={styles.hasAccount}>
+                        <hr />
+
+                        <div>
+                            <span>Ya tienes una cuenta?</span>
+                            <button>
+                                <span onClick={() => Router.push('/login')}>Iniciar sesión</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
-
             </div>
-        </div>
         </>
-    
     );
 }
 
