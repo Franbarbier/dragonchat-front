c30757b8d3ad35c5c6c0d2082c9c55f21b1ebc70
NicolasArias1 <ariasespinoza23@gmail.com>
2023-11-09 18:36:03 -0300

    minor style fix

--- components/MainCont/MainCont.tsx
+++ components/MainCont/MainCont.tsx
@@ -1,26 +1,27 @@
+import { MAINTENANCE } from '../../constants/index';
 import styles from './MainCont.module.css';
 
 export interface IMainCont {
     width?: number | null;
     maxWidth?: number | null;
-    children : React.ReactElement<any, any>;
-    style? : object;
+    children: React.ReactElement<any, any>;
+    style?: object;
 }
 
 
 
 // interface contactosArr extends Array<ContactInfo>{}
 
-const MainCont: React.FC<IMainCont> = ({ width = 50, maxWidth = 1200, children, style={} }) => {
+const MainCont: React.FC<IMainCont> = ({ width = 50, maxWidth = 1200, children, style = {} }) => {
 
-   
+
     return (
-        <div className={styles.main_cont} style={style}>
-            <div style={{'width' : `${width}vw`, 'maxWidth' : `${maxWidth}px` }}>
+        <div className={MAINTENANCE ? styles.main_cont_maintence : styles.main_cont} style={style}>
+            <div style={{ 'width': `${width}vw`, 'maxWidth': `${maxWidth}px` }}>
                 {children}
             </div>
         </div>
-    
+
     );
 }
 
