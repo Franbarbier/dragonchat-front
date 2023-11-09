eff4028558cfcf713c932894d0bcc565baa3f926
NicolasArias1 <ariasespinoza23@gmail.com>
2023-11-09 20:07:05 -0300

    fix pr comments part 2

--- components/CustomColorBtn/CustomColorBtn.tsx
+++ components/CustomColorBtn/CustomColorBtn.tsx
@@ -1,19 +1,24 @@
 import styles from './CustomColorBtn.module.css';
 
 export interface ICustomColorBtn {
-    text : string;
+    text: string;
     type?: "button" | "submit" | "reset";
     backgroundColorInit: string;
     backgroundColorEnd: string;
     disable?: boolean;
     borderColor: string,
-    onClick: (arg:React.MouseEvent<HTMLElement>) => void;
+    onClick: (arg: React.MouseEvent<HTMLElement>) => void;
 }
 
-const CustomColorBtn: React.FC<ICustomColorBtn> = ({ text, type="button", backgroundColorInit, backgroundColorEnd, borderColor, onClick, disable=false }) => {
-
-
-    return <button type={type} className={`${styles.customColorBtn} ${disable && styles.disabled}`} onClick={ (e)=> { if(!disable){ onClick(e) }}} style={{background: `linear-gradient(0, ${backgroundColorInit}, ${backgroundColorEnd})`, border: `1px solid ${borderColor}`}}>{text}</button>;
-}
+const CustomColorBtn: React.FC<ICustomColorBtn> = ({ text, type = "button", backgroundColorInit, backgroundColorEnd, borderColor, onClick, disable = false }) => (
+    <button
+        type={type}
+        className={`${styles.customColorBtn} ${disable && styles.disabled}`}
+        onClick={(e) => { if (!disable) { onClick(e) } }}
+        style={{ background: `linear-gradient(0, ${backgroundColorInit}, ${backgroundColorEnd})`, border: `1px solid ${borderColor}` }}
+    >
+        {text}
+    </button>
+)
 
 export default CustomColorBtn;
