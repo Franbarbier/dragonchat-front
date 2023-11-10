c5cdcdf8d3b278bb84ae32676009308b2bcd1273
Franbarbier <48157540+Franbarbier@users.noreply.github.com>
2023-11-10 09:47:34 -0300

    Mantenimiento on/off (#92)
    * mantenimiento on/off
    
    * rm env
    
    * minor style fix
    
    * fix pr comments part 1
    
    * fix pr comments part 2
    
    * fix duplicated code in middleware
    
    * fix spelling
    
    ---------
    
    Co-authored-by: NicolasArias1 <ariasespinoza23@gmail.com>
    Co-authored-by: NicolasArias1 <56004709+NicolasArias1@users.noreply.github.com>

---
+++ components/Maintenance/Maintenance.tsx
@@ -0,0 +1,16 @@
+import { FILE } from '../../enums';
+import styles from './Maintenance.module.css';
+
+const Maintenance: React.FC = ({ }) => (
+    <div className={styles.maintCont}>
+        <img src={FILE.MAINTENANCE} />
+        <div>
+            <p>{"¡Hey, estamos en mantenimiento!"}</p>
+            <p>{"Pero dentro de muy poco estamos de nuevo funcionando"}</p>
+            <br />
+            <p>{"Regresa dentro de un rato para continuar contactando y dominando tu mercado."}</p>
+        </div>
+    </div>
+)
+
+export default Maintenance;
