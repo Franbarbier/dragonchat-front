1b486213bfc938633ca5fadd3d264daeba93f18d
Francisco <ffranbarbier@gmail.com>
2023-11-14 23:29:48 -0300

    css change limit message

--- components/BoxDialog/BoxDialog.tsx
+++ components/BoxDialog/BoxDialog.tsx
@@ -11,6 +11,9 @@ interface Props {
 const BoxDialog: React.FC<Props> = ({ setRenderDialog }) => {
     return (
         <div className={styles.box_dialog}>
+            <div className={styles.close_box} onClick={ ()=> setRenderDialog(false) } >
+                <img src="/close.png" alt="close_box_dialog" />
+            </div>
             <div className={styles.box_dialog_arrow} ></div>
             <div className={styles.box_dialog_content} >
                 <span>
@@ -22,26 +25,15 @@ const BoxDialog: React.FC<Props> = ({ setRenderDialog }) => {
                 </span>
                 <CustomColorBtn
                     type="submit"
-                    text="Entendido"
-                    backgroundColorInit="#13013780"
-                    backgroundColorEnd="#13013780"
-                    borderColor="var(--newViolet)"
-                    onClick={()=>{ 
-                        setRenderDialog(false)
-                     } }
-                    disable={ false }
+                    text="PASARME A PRO"
+                    backgroundColorInit="#c21c3b"
+                    backgroundColorEnd="#f9bd4f"
+                    borderColor="#e17846"
+                    onClick={ (e)=>{ 
+                        // Router.push("/checkout")
+                        window.location.href = `${HOST_URL}${ROUTES.CHECKOUT}`;
+                    } }
                 />
-                <CustomColorBtn
-                        type="submit"
-                        text="version ilimitada"
-                        backgroundColorInit="#c21c3b"
-                        backgroundColorEnd="#f9bd4f"
-                        borderColor="#e17846"
-                        onClick={ (e)=>{ 
-                            // Router.push("/checkout")
-                            window.location.href = `${HOST_URL}${ROUTES.CHECKOUT}`;
-                        } }
-                    />
             </div>
         </div>
     );
