d228bb4ba3f12570990e95c98cb465b9fd9d0d86
Francisco <ffranbarbier@gmail.com>
2024-04-23 00:15:45 -0300

    no index search engine

--- pages/_middleware/noIndex.js
+++
@@ -1,7 +0,0 @@
-
-// import { NextApiRequest, NextApiResponse } from 'next';
-
-// export default function noIndexMiddleware(req: NextApiRequest, res: NextApiResponse, next: () => void) {
-//     res.setHeader('X-Robots-Tag', 'noindex');
-//     next();
-// }
