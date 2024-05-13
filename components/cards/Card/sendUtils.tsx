7866ef52e5be6d00023adaeb1c6fdf7b10343887
Francisco <ffranbarbier@gmail.com>
2024-05-13 19:43:29 -0300

    add number code for mex arg chi

--- components/cards/Card/sendUtils.tsx
+++ components/cards/Card/sendUtils.tsx
@@ -1,44 +1,88 @@
 
 export const globalName = (message:string[]) => {
 
-    const variations = [
-        /\[name\]/gi,
-        /\[Name\]/gi,
-        /\[NAME\]/gi,
-        /\(name\)/gi,
-        /\(Name\)/gi,
-        /\(NAME\)/gi,
-        /\{name\}/gi,
-        /\{Name\}/gi,
-        /\{NAME\}/gi,
-        /\[nombre\]/gi,
-        /\[Nombre\]/gi,
-        /\[NOMBRE\]/gi,
-        /\(nombre\)/gi,
-        /\(Nombre\)/gi,
-        /\(NOMBRE\)/gi,
-        /\{nombre\}/gi,
-        /\{Nombre\}/gi,
-        /\{NOMBRE\}/gi,
-        /\[user\]/gi,
-        /\(user\)/gi,
-        /\{user\}/gi,
-        /\[usuario\]/gi,
-        /\(usuario\)/gi,
-        /\{usuario\}/gi,
-        // Add more variations here if needed
-      ];
-    
-      // Loop through each message in the array
-  const normalizedMessages = message.map(m => {
-    // Replace each variation with '[name]' in the current message
-    variations.forEach(variation => {
-      m = m.replace(variation, '[name]');
-    });
-    return m;
+  const variations = [
+      /\[name\]/gi,
+      /\[Name\]/gi,
+      /\[NAME\]/gi,
+      /\(name\)/gi,
+      /\(Name\)/gi,
+      /\(NAME\)/gi,
+      /\{name\}/gi,
+      /\{Name\}/gi,
+      /\{NAME\}/gi,
+      /\[nombre\]/gi,
+      /\[Nombre\]/gi,
+      /\[NOMBRE\]/gi,
+      /\(nombre\)/gi,
+      /\(Nombre\)/gi,
+      /\(NOMBRE\)/gi,
+      /\{nombre\}/gi,
+      /\{Nombre\}/gi,
+      /\{NOMBRE\}/gi,
+      /\[user\]/gi,
+      /\(user\)/gi,
+      /\{user\}/gi,
+      /\[usuario\]/gi,
+      /\(usuario\)/gi,
+      /\{usuario\}/gi,
+      // Add more variations here if needed
+    ];
+  
+    // Loop through each message in the array
+const normalizedMessages = message.map(m => {
+  // Replace each variation with '[name]' in the current message
+  variations.forEach(variation => {
+    m = m.replace(variation, '[name]');
   });
+  return m;
+});
 
-  return normalizedMessages;
+return normalizedMessages;
 
 }
-  
+
+
+export const validationCode = (numero:string) => {
+
+let validatedNum = numero
+let caracteristca = validatedNum.slice(0, 2)
+let rest = validatedNum.slice(2)
+
+// if starts with 54
+if (validatedNum.startsWith("54")) {
+  if (rest.charAt(0) != "9") {
+      validatedNum = caracteristca + "9" + rest
+  }else{
+     if (validatedNum.length < 13) {
+      validatedNum = caracteristca + "9" + rest
+     }
+  }
+}
+// 52
+else if (validatedNum.startsWith("52")) {
+  if (rest.charAt(0) != "1") {
+    validatedNum = caracteristca + "1" + rest
+  }else{
+    if (validatedNum.length < 13) {
+      validatedNum = caracteristca + "1" + rest
+    }
+  }
+
+}
+// 56
+else if (validatedNum.startsWith("56")) {
+  // console log first character of "Rest"
+  if (rest.charAt(0) != "9") {
+    validatedNum = caracteristca + "9" + rest
+}else{
+   if (validatedNum.length < 11) {
+    validatedNum = caracteristca + "9" + rest
+   }
+}
+
+}
+
+
+return validatedNum
+}
