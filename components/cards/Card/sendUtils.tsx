cb06af19469f5070e384a3e3b03acbc34a794ea7
Alfredo Puerta <alfredopuerta.cl@gmail.com>
2024-05-14 00:07:57 -0400

    remove log

--- components/cards/Card/sendUtils.tsx
+++ components/cards/Card/sendUtils.tsx
@@ -26,12 +26,9 @@ export const globalName = (message:string[]) => {
       /\[usuario\]/gi,
       /\(usuario\)/gi,
       /\{usuario\}/gi,
-      // Add more variations here if needed
     ];
   
-    // Loop through each message in the array
 const normalizedMessages = message.map(m => {
-  // Replace each variation with '[name]' in the current message
   variations.forEach(variation => {
     m = m.replace(variation, '[name]');
   });
