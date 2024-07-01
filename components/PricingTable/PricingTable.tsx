9447c12bde7a56073ee23ad8c05dbc118bae5f3f
Francisco <ffranbarbier@gmail.com>
2024-07-01 20:02:23 -0300

    txt benefits

--- components/PricingTable/PricingTable.tsx
+++ components/PricingTable/PricingTable.tsx
@@ -16,6 +16,22 @@ const PricingTable: React.FC<IPricingTable> = ({ }) => {
             <div>
                 <h2>Pricing</h2>
             </div>
+            <div>
+                <div className={styles.infoCont}>
+                    <h3>Continúa en la vanguardia de la mensajería</h3>
+                    <ul>
+                        <li><img src="cierto.png"/>Importar contactos .csv.</li>
+                        <li><img src="cierto.png"/>Entregabilidad del 100%.</li>
+                        <li><img src="cierto.png"/>Sistema antiblocker.</li>
+                        <li><img src="cierto.png"/>Mensajes ilimitados.</li>
+                        <li style={{"opacity":"0.7"}}><img src="cierto.png"/>Adjuntar archivos en los envíos. (próximamente)</li>
+                        <li style={{"color":"#b5b5b5"}}><img src="cierto.png"/>Enviar archivos. (próximamente)</li>
+                        <li style={{"color":"#b5b5b5"}}><img src="cierto.png"/>Creador de Secuencias. (próximamente)</li>
+                        <li style={{"color":"#b5b5b5"}}><img src="cierto.png"/>Base de Contactos. (próximamente)</li>
+                        <li style={{"color":"#b5b5b5"}}><img src="cierto.png"/>Métricas y CRM. (próximamente)</li>
+                    </ul>
+                </div>
+            </div>
             <div className={styles.pricingsCont}>
                 <div>
                     <div>
